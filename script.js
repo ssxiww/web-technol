29e8158920670d11588daffb3c82c1e12d66e18f
ssxiww <nnassttyya@gmail.com>
2024-12-16 11:10:39 +0500

    1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', init)
+    document.addEventListener('DOMContentLoaded', init);
 } else {
-    init()
+    init();
 }
 
 function init() {
@@ -14,111 +14,104 @@ function init() {
                 hasChildren: true,
                 items: [
                     {
-                        name: 'Ulgran1',
+                        name: 'Ulgran',
                         hasChildren: true,
                         items: [
-                            {
-                                name: 'SMT1',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT2',
-                                hasChildren: false,
-                                items: []
-                            }
+                            { name: 'Smth', hasChildren: false, items: [] },
+                            { name: 'Smth', hasChildren: false, items: [] }
                         ]
                     },
                     {
-                        name: 'Ulgran2',
+                        name: 'Vigro Mramor',
+                        hasChildren: false,
+                        items: []
+                    },
+                    {
+                        name: 'Handmade',
                         hasChildren: true,
                         items: [
-                            {
-                                name: 'SMT3',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT4',
-                                hasChildren: false,
-                                items: []
-                            }
+                            { name: 'Smth', hasChildren: false, items: [] },
+                            { name: 'Smth', hasChildren: false, items: [] }
                         ]
+                    },
+                    {
+                        name: 'Vigro Glass',
+                        hasChildren: false,
+                        items: []
                     }
                 ]
-            },{
+            },
+            {
                 name: 'Фильтры',
                 hasChildren: true,
                 items: [
                     {
-                        name: 'Ulgran3',
+                        name: 'Ulgran',
                         hasChildren: true,
                         items: [
-                            {
-                                name: 'SMT5',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT6',
-                                hasChildren: false,
-                                items: []
-                            }
+                            { name: 'Smth', hasChildren: false, items: [] },
+                            { name: 'Smth', hasChildren: false, items: [] }
                         ]
+                    },
+                    {
+                        name: 'Vigro Mramor',
+                        hasChildren: false,
+                        items: []
                     }
                 ]
             }
         ]
-    }
+    };
 
-
-    const items = new ListItems(document.getElementById('list-items'), data)
-
-
-  /*  items.render()*/
-    items.init()
-
-    /*console.log(items.renderTest(data));*/
+    const items = new ListItems(document.getElementById('list-items'), data);
+    items.render();
+    items.init();
 
     function ListItems(el, data) {
         this.el = el;
         this.data = data;
 
         this.init = function () {
-            const parents = this.el.querySelectorAll('[data-parent]')
-
-            parents.forEach(parent => {
-                const open = parent.querySelector('[data-open]')
-
-                open.addEventListener('click', () => this.toggleItems(parent) )
-            })
-        }
+            this.el.addEventListener('click', (event) => {
+                const arrow = event.target.closest('[data-open]');
+                if (arrow) {
+                    const parent = arrow.closest('[data-parent]');
+                    this.toggleItems(parent);
+                }
+            });
+        };
 
         this.render = function () {
-            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
-        }
+            this.el.innerHTML = this.renderParent(this.data);
+        };
 
+        // Рендеринг узлов
         this.renderParent = function (data) {
-            //проверка всех элементов на hasChildren
-            //если hasChildren, то запускаем renderParent
-            //если !hasChildren, то запускаем renderChildren
-            //возвращает рендер родительского элемента
+            let html = `
+                <div class="list-item" data-parent>
+                    <div class="list-item__inner">
+                        <div class="list-item__arrow-container">
+                            ${data.hasChildren ? '<img class="list-item__arrow" src="img/chevron-down.png" alt="chevron-down" data-open>' : ''}
+                        </div>
+                        <img class="list-item__folder" src="img/folder.png" alt="folder">
+                        <span class="list-item__text">${data.name}</span>
+                    </div>`;
 
-        }
+            if (data.hasChildren) {
+                html += '<div class="list-item__items">';
+                data.items.forEach(item => {
+                    html += this.renderParent(item); // Рекурсия
+                });
+                html += '</div>';
+            }
 
-        this.renderChildren = function (data) {
-            //вовзращает рендер элемента без вложенности
-        }
+            html += '</div>';
+            return html;
+        };
+
 
         this.toggleItems = function (parent) {
-            parent.classList.toggle('list-item_open')
-        }
-
-/*        this.renderTest = function (data) {
-            return `
-            <div class="test">${data.name}</div>
-            `
-        }*/
+            parent.classList.toggle('list-item_open');
+        };
     }
-
 }
