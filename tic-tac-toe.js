3dfa54619d222ec122c66da61df9d903c88137d0
ssxiww <nnassttyya@gmail.com>
2024-12-16 17:00:13 +0500

    переделала1

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,98 +1,38 @@
-export class TicTacToe {
-    constructor(options) {
-        this.el = options.el;
-        this.onMove = options.onMove;
-        this.currentPlayer = 'X';
-        this.board = [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
-        ];
-        this.moves = 0;
-        this.gameOver = false;
-    }
+import {TicTacToe} from "./components/TicTacToe.js";
 
-    static init(options) {
-        return new TicTacToe(options);
-    }
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init)
+} else {
+    init()
+}
 
-    startGame() {
-        this.el.addEventListener('click', this.handleCellClick.bind(this));
-        this.onMove(true);
-    }
-
-    restartGame() {
-        this.resetBoard();
-        this.currentPlayer = 'X';
-        this.moves = 0;
-        this.gameOver = false;
-        this.onMove(true);
-        this.updateBoard();
-    }
-
-    handleCellClick(event) {
-        if (this.gameOver) return;
-
-        const cell = event.target;
-        const row = parseInt(cell.getAttribute('data-row')) - 1;
-        const col = parseInt(cell.getAttribute('data-col')) - 1;
-
-        if (this.board[row][col] === '') {
-            this.board[row][col] = this.currentPlayer;
-            cell.innerText = this.currentPlayer;
-            this.moves++;
-
-            if (this.checkWin(row, col)) {
-                alert(`Игрок ${this.currentPlayer} выиграл!`);
-                this.gameOver = true;
-            } else if (this.checkDraw()) {
-                alert('Ничья!');
-                this.gameOver = true;
-            } else {
-                this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
-                this.onMove(this.currentPlayer === 'X');
-            }
+function init() {    
+    const moveEl = document.getElementById('move-value')
+    
+    const onMove = (isXTurn) => {
+        let currentMove
+        
+        if (isXTurn) {
+            currentMove = 'X'
+        } else {
+            currentMove = 'O'
         }
+
+        moveEl.innerText = currentMove
     }
 
-    checkWin(row, col) {
-        const player = this.board[row][col];
+    const game = TicTacToe.init(
+        {
+            el: document.getElementById('tic-tac-toe'),
+            onMove,
+        }
+    )
+    
+    game.startGame()
+    
+    const restartBtn = document.getElementById('restart-btn')
 
-        // Проверка строки
-        if (this.board[row].every(cell => cell === player)) return true;
-
-        // Проверка столбца
-        if (this.board.every(r => r[col] === player)) return true;
-
-        // Проверка диагоналей
-        if (row === col && this.board.every((r, i) => r[i] === player)) return true;
-        if (row + col === 2 && this.board.every((r, i) => r[2 - i] === player)) return true;
-
-        return false;
-    }
-
-    checkDraw() {
-        return this.moves === 9;
-    }
-
-    updateBoard() {
-        const cells = this.el.querySelectorAll('.tic-tac-toe__ceil');
-        cells.forEach(cell => {
-            const row = parseInt(cell.getAttribute('data-row')) - 1;
-            const col = parseInt(cell.getAttribute('data-col')) - 1;
-            cell.innerText = this.board[row][col];
-        });
-    }
-
-    resetBoard() {
-        this.board = [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
-        ];
-        const cells = this.el.querySelectorAll('.tic-tac-toe__ceil');
-        cells.forEach(cell => {
-            cell.innerText = '';
-        });
-    }
+    restartBtn.addEventListener('click', () => {
+        game.restartGame()
+    })
 }
