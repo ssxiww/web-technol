a59333fa5d11e704c8d87376c06ad3e3c8311161
ssxiww <nnassttyya@gmail.com>
2024-12-18 00:42:48 +0500

    готово

---
+++ Assets/js/main.js
@@ -0,0 +1,14 @@
+// const burgerMenu = document.getElementById('burgerMenu');
+// const mobileMenu = document.getElementById('mobileMenu');
+// const closeMenu = document.getElementById('closeMenu');
+// const header = document.getElementById('head-model');
+
+// burgerMenu.addEventListener('click', () => {
+//     mobileMenu.classList.add('show');
+//     header.classList.add('show');
+// });
+
+// closeMenu.addEventListener('click', () => {
+//     mobileMenu.classList.remove('show');
+//     header.classList.remove('show');
+// });
